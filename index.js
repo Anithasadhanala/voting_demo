19a5908c756d4612225209a6aa28e3c093abc8ac
Anithasadhanala <20mh1a4248.acoe.edu.in>
2023-03-08 17:58:11 +0530

    second

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ const db = phpmyadmin.createPool({
 
 app.use(express.static(path.join(__dirname+'/public/build')))
 
-console.log("testing..")
+
+console.log("testing....")
 app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
